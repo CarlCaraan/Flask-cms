4185c372c6e420c159d9bee1e3560e06d9ad0e7a
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-01-14 20:46:44 +0800

    1/14/22

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Post
+from .models import Post, User
 from . import db
 
 views = Blueprint("views", __name__)
@@ -30,7 +30,6 @@ def create_post():
 
     return render_template('create_post.html', user=current_user)
 
-
 # ========= DELETE POST =========
 @views.route("/delete-post/<id>")
 @login_required
@@ -47,3 +46,15 @@ def delete_post(id):
         flash('Post Deleted Successfully', category='success')
 
     return redirect(url_for('views.home'))
+
+# ========= VIEW POST =========
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        flash('No user with that username exists.', category='error')
+        return redirect(url_for('views.home'))
+
+    posts = user.posts
+    return render_template('posts.html', user=current_user, posts=posts, username=username)
